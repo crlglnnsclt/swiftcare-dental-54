db95a77c357b8b0ecbdd0cfa1948b35cf7febfc2
crlglnnsclt <83820117+crlglnnsclt@users.noreply.github.com>
2025-09-07 16:31:37 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { PatientProvider } from './lib/PatientContext'; // <-- import your context provider
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <PatientProvider>
+    <App />
+  </PatientProvider>
+);
